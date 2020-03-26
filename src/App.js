c49fecf264576dbb98e72a5f0d0d288ef12f386f
Victor Cordeiro <victor.hugo@systemhope.com.br>
2020-03-26 14:54:18 -0300

    Pagina de login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import './App.css'
-import logo from './logo.svg'
+import './global.css'
+import Logon from './pages/Logon'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Omnistack</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <Logon />
 }
 
 export default App
